ddd3cd75bf0a1bfd37e5f796b71d658dca9ffd1a
chat-bot-tom <chatbottomyvr@gmail.com>
2026-03-23 00:16:32 -0700

    refactor: enhance TypeScript support for Stripe integration and improve workflow reliability

--- components/ErrorDisplay.tsx
+++ components/ErrorDisplay.tsx
@@ -19,11 +19,14 @@ export const ErrorDisplay: React.FC<ErrorDisplayProps> = ({ message, onClear }) 
         </div>
       </div>
       {onClear && (
-         <button 
-            onClick={onClear} 
+         <button
+            type="button"
+            onClick={onClear}
+            title="Dismiss error"
+            aria-label="Dismiss error"
             className="p-2 text-red-500 hover:text-red-700 transition-colors"
         >
-            <svg className="h-5 w-5" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor"><path fillRule="evenodd" d="M10 18a8 8 0 1 0 0-16 8 8 0 0 0 0 16ZM8.28 7.22a.75.75 0 0 0-1.06 1.06L8.94 10l-1.72 1.72a.75.75 0 1 0 1.06 1.06L10 11.06l1.72 1.72a.75.75 0 1 0 1.06-1.06L11.06 10l1.72-1.72a.75.75 0 0 0-1.06-1.06L10 8.94 8.28 7.22Z" clipRule="evenodd" /></svg>
+            <svg className="h-5 w-5" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true"><path fillRule="evenodd" d="M10 18a8 8 0 1 0 0-16 8 8 0 0 0 0 16ZM8.28 7.22a.75.75 0 0 0-1.06 1.06L8.94 10l-1.72 1.72a.75.75 0 1 0 1.06 1.06L10 11.06l1.72 1.72a.75.75 0 1 0 1.06-1.06L11.06 10l1.72-1.72a.75.75 0 0 0-1.06-1.06L10 8.94 8.28 7.22Z" clipRule="evenodd" /></svg>
         </button>
       )}
     </div>
